e922967b7157c07786a7cf913a3e525de101e68a
wyllman <wyllman@gmail.com>
2014-08-28 11:07:39 +0100

    segundo intento

---
+++ js/csv.js
@@ -0,0 +1,68 @@
+// See http://en.wikipedia.org/wiki/Comma-separated_values
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+$(document).ready(function() {
+   $("button").click(function() {
+     calculate();
+   });
+ });
+
+function calculate() {
+  var result;
+  var original = document.getElementById("original");
+  var temp = original.value;
+  
+  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+  
+  var lines = temp.split(/\n+\s*/);
+  var commonLength = NaN;
+  var r = [];
+  // Template using underscore
+  var row = "<% _.each(items, function(name) { %>"     +
+            "                    <td><%= name %></td>" +
+            "              <% }); %>";
+
+  if (window.localStorage) localStorage.original  = temp;
+  
+  for(var t in lines) {
+    var temp = lines[t];
+    var m = temp.match(regexp);
+    var result = [];
+    var error = false;
+    
+    if (m) {
+      if (commonLength && (commonLength != m.length)) {
+        //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
+        error = true;
+      }
+      else {
+        commonLength = m.length;
+        error = false;
+      }
+      for(var i in m) {
+        var removecomma = m[i].replace(/,\s*$/,'');
+        var remove1stquote = removecomma.replace(/^\s*"/,'');
+        var removelastquote = remove1stquote.replace(/"\s*$/,'');
+        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
+        result.push(removeescapedquotes);
+      }
+      var tr = error? '<tr class="error">' : '<tr>';
+      r.push(tr+_.template(row, {items : result})+"</tr>");
+    }
+    else {
+      alert('ERROR! row '+temp+' does not look as legal CSV');
+      error = true;
+    }
+  }
+  r.unshift('<p>\n<table class="center" id="result">');
+  r.push('</table>');
+  //alert(r.join('\n')); // debug
+  finaltable.innerHTML = r.join('\n');
+}
+
+window.onload = function() {
+  // If the browser supports localStorage and we have some stored data
+  if (window.localStorage && localStorage.original) {
+    document.getElementById("original").value = localStorage.original;
+  }
+};
